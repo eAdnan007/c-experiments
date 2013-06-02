9e9f51de8b59ce4340b7f4a5fa61b37027b98c89
Mohaimenul Haque Adnan <adnan007.id@gmail.com>
2013-06-03 02:14:41 +0600

    This version is for submitting in scarky

--- score.c
+++ score.c
@@ -3,15 +3,13 @@
 int main(){
     int lines, c;
 
-    printf("Lines:");
     scanf("%d", &lines);
 
-    printf("Provide your input(as \"Total opposition runs\" \"Current batting runs\" \"Balls left\")\n");
     int total_runs[lines], current_runs[lines], balls_left[lines];
     for(c = 1; c<=lines; c++){
         scanf("%d %d %d", &total_runs[c], &current_runs[c], &balls_left[c]);
     }
-
+    printf("\n");
     int balls_played;
     double cur_run_rate, req_run_rate, overs_played, overs_left;
     for(c = 1; c<=lines; c++){
@@ -20,7 +18,8 @@ int main(){
         overs_left = (double) balls_left[c]/6;
         cur_run_rate = (double) current_runs[c]/overs_played;
         req_run_rate = (double) (total_runs[c]-current_runs[c]+1)/overs_left;
-        printf("%0.02lf %0.02lf\n", cur_run_rate, req_run_rate);
+        printf("%0.02lf %0.02lf", cur_run_rate, req_run_rate);
+        printf("\n");
     }
 
     return 0;
