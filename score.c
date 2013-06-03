ea14b5d6c18e0fcd4a9626b2cd030d0493401fe5
eAdnan007 <adnan007.id@gmail.com>
2013-06-03 10:01:03 +0600

    Used "line" variable name instead of "c" to understand it better.

--- score.c
+++ score.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int lines, c;
+    int lines, line;
 
     scanf("%d", &lines);
 
     int total_runs[lines], current_runs[lines], balls_left[lines];
-    for(c = 1; c<=lines; c++){
-        scanf("%d %d %d", &total_runs[c], &current_runs[c], &balls_left[c]);
+    for(line = 1; line<=lines; line++){
+        scanf("%d %d %d", &total_runs[line], &current_runs[line], &balls_left[line]);
     }
     printf("\n");
     int balls_played;
     double cur_run_rate, req_run_rate, overs_played, overs_left;
-    for(c = 1; c<=lines; c++){
-        balls_played = 300 - balls_left[c];
+    for(line = 1; line<=lines; line++){
+        balls_played = 300 - balls_left[line];
         overs_played = (double) balls_played/6;
-        overs_left = (double) balls_left[c]/6;
-        cur_run_rate = (double) current_runs[c]/overs_played;
-        req_run_rate = (double) (total_runs[c]-current_runs[c]+1)/overs_left;
+        overs_left = (double) balls_left[line]/6;
+        cur_run_rate = (double) current_runs[line]/overs_played;
+        req_run_rate = (double) (total_runs[line]-current_runs[line]+1)/overs_left;
         printf("%0.02lf %0.02lf", cur_run_rate, req_run_rate);
         printf("\n");
     }
